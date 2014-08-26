7626a144c312653ef8a3683399ee1ae9f040620a
geno <gn.rmld@gmail.com>
2014-08-26 13:46:45 +0800

    added SimpleDBI library, downloaded router.php and database.php, configured bootstrap.php, added board.sql, modified core.php

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+
+
 // application
 require_once APP_DIR.'app_controller.php';
 require_once APP_DIR.'app_model.php';
@@ -10,6 +12,11 @@ require_once HELPERS_DIR.'html_helper.php';
 
 // config
 require_once CONFIG_DIR.'log.php';
+require_once CONFIG_DIR.'router.php';
+require_once CONFIG_DIR.'database.php';
+
+//vendor
+require_once VENDOR_DIR.'SimpleDBI/src/SimpleDBI.php';
 
 spl_autoload_register(function($name) {
     $filename = Inflector::underscore($name) . '.php';
